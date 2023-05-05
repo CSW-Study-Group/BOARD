ce19c84f17d4d85d3b7ecdf36214cd7d5014d589
[pyola4981] <[pyola4981@naver.com]>
2023-05-05 19:00:33 +0900

    test용 commit

--- FrontEnd/public/js/user/profile.js
+++ FrontEnd/public/js/user/profile.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const update_btn = document.querySelector("#update_btn");
+const attend_btn = document.querySelector("#attend_btn");
 const fieldset = document.querySelector('fieldset');
 const form_show = document.getElementById('show');
 
 update_btn.addEventListener("click", updateProfile);
+attend_btn.addEventListener("click", attendCheck);
 
 function updateProfile() {
     const is_disabled = fieldset.getAttribute('disabled') !== null;
@@ -53,4 +55,42 @@ function updateProfile() {
         update_btn.innerText = "프로필 편집";
         fieldset.disabled = true;
     }
+}
+
+function attendCheck() {
+    fetch("/user/attend", {
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json',
+            'authorization': localStorage.getItem('access_token')
+        },
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        if(res.code === 200) {
+            location.href = "/";
+        } else if(res.code === 419){ // Access Token has expired.
+            fetch("/user/token/refresh", {
+                    headers: { 'authorization': localStorage.getItem('refresh_token') }
+                })
+                .then((res) => res.json())
+                .then((res) => {
+                    if(res.code === 419) {
+                        alert(res.message);
+                        localStorage.removeItem('access_token');
+                        localStorage.removeItem('refresh_token');
+                        window.location.href = "/user/login";
+                    } else {
+                        alert(res.message);
+                        localStorage.setItem('access_token', res.access_token);
+                    }
+                })
+        } else { // 401 or 500
+            alert(res.message);
+            location.href = "/";
+        }
+    })
+    .catch((err) => {
+        alert('An error occurred while processing your request. Please try again later.');
+    });
 }
