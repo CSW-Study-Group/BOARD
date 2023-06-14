94b78ab389291e0cc5cda1f9c05514759a1dae1e
SoN-B <wjdgh2777@naver.com>
2023-06-14 19:21:35 +0900

    Refactor: 백엔드 프로젝트 eslint 적용

--- BackEnd/src/middleware/verifyJWT.js
+++ BackEnd/src/middleware/verifyJWT.js
@@ -5,26 +5,26 @@ const config = require('config');
 const ACCESS_SECRET_KEY = config.get('JWT.access_secret_key');
 
 exports.auth = (req, res, next) => {
-    // 인증 완료
-    try {
-        // 요청 헤더에 저장된 토큰(req.headers.authorization)과 비밀키를 사용하여 토큰을 req.decoded에 반환
-        req.decoded = jwt.verify(req.headers.authorization, ACCESS_SECRET_KEY);
-        return next();
-    } catch (error) {
-        // 인증 실패
-        // 유효시간이 초과된 경우
-        if (error.name === 'TokenExpiredError') {
-            return res.status(419).json({
-                code: 419,
-                message: 'Access token has expired.',
-            });
-        }
-        // 토큰의 비밀키가 일치하지 않는 경우
-        if (error.name === 'JsonWebTokenError') {
-            return res.status(401).json({
-                code: 401,
-                message: 'Access token is invalid.',
-            });
-        }
+  // 인증 완료
+  try {
+    // 요청 헤더에 저장된 토큰(req.headers.authorization)과 비밀키를 사용하여 토큰을 req.decoded에 반환
+    req.decoded = jwt.verify(req.headers.authorization, ACCESS_SECRET_KEY);
+    return next();
+  } catch (error) {
+    // 인증 실패
+    // 유효시간이 초과된 경우
+    if (error.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        code: 419,
+        message: 'Access token has expired.',
+      });
     }
+    // 토큰의 비밀키가 일치하지 않는 경우
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(401).json({
+        code: 401,
+        message: 'Access token is invalid.',
+      });
+    }
+  }
 };
--- BackEnd/src/models/post.js
+++ BackEnd/src/models/post.js
@@ -3,61 +3,61 @@
 const Sequelize = require('sequelize');
 
 module.exports = class Post extends Sequelize.Model {
-    static init(sequelize) {
-        return super.init(
-            {
-                id: {
-                    type: Sequelize.INTEGER,
-                    autoIncrement: true,
-                    allowNull: false,
-                    unique: true,
-                    primaryKey: true,
-                },
-                title: {
-                    type: Sequelize.TEXT,
-                    allowNull: false,
-                },
-                content: {
-                    type: Sequelize.TEXT,
-                    allowNull: false,
-                },
-                view: {
-                    type: Sequelize.INTEGER,
-                    allowNull: false,
-                },
-                recommand: {
-                    type: Sequelize.INTEGER,
-                    allowNull: false,
-                    defaultValue: 0,
-                },
-                created_at: {
-                    type: Sequelize.DATE,
-                    allowNull: false,
-                    defaultValue: Sequelize.NOW,
-                    updated_at: false
-                },
-                updated_at: {
-                    type: Sequelize.DATE,
-                    allowNull: true,
-                    defaultValue: null,
-                    updated_at: false
-                }
-            },
-            {
-                sequelize,
-                timestamps: false,
-                paranoid: true,
-                modelName: 'Post',
-                tableName: 'post_info',
-                charset: 'utf8mb4',
-                collate: 'utf8mb4_unicode_ci',
-            },
-        );
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          allowNull: false,
+          unique: true,
+          primaryKey: true,
+        },
+        title: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        content: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        view: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        recommand: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 0,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+          updated_at: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: true,
+          defaultValue: null,
+          updated_at: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        paranoid: true,
+        modelName: 'Post',
+        tableName: 'post_info',
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci',
+      },
+    );
+  }
 
-    static associate(db) {
-        db.Post.belongsTo(db.User, { foreignKey: 'user_id', targetKey: 'id', onDelete: 'cascade', onUpdate: 'cascade' });
-        db.Post.belongsToMany(db.User, { through: 'user_post', foreignKey: 'post_id', otherKey: 'user_id' });
-        db.Post.hasMany(db.Comment, { foreignKey: 'post_id', sourceKey: 'id', onDelete: 'cascade', onUpdate: 'cascade' });
-    }
+  static associate(db) {
+    db.Post.belongsTo(db.User, { foreignKey: 'user_id', targetKey: 'id', onDelete: 'cascade', onUpdate: 'cascade' });
+    db.Post.belongsToMany(db.User, { through: 'user_post', foreignKey: 'post_id', otherKey: 'user_id' });
+    db.Post.hasMany(db.Comment, { foreignKey: 'post_id', sourceKey: 'id', onDelete: 'cascade', onUpdate: 'cascade' });
+  }
 };
