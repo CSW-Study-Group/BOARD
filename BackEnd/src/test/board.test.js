8ced1d3532811031fc42e933fd6cc65de6e17f9f
SoN-B <wjdgh2777@naver.com>
2023-06-21 20:25:53 +0900

    Feat: 게시글 조회 테스트 코드

--- BackEnd/src/test/board.test.js
+++ BackEnd/src/test/board.test.js
@@ -3,8 +3,17 @@
 const request = require('supertest');
 const { app } = require('../../server');
 
+const board = require('../controllers/board');
+
 const { config, chalk } = require('../../loaders/module');
 
+/**
+ * * 게시글 목록 조회 테스트
+ * 1. 게시글이 없을 경우
+ * 2. 페이지가 1000 이상일 경우
+ * 3. limit 값이 5, 10, 20이 아닐 경우
+ * 4. 검색어가 있을 경우
+ */
 describe('getBoard', () => {
   let server;
 
@@ -16,11 +25,11 @@ describe('getBoard', () => {
     server.close(done);
   });
 
-  test(`should return ${chalk.green(200)} and render post/index page if ${chalk.blue('data is not exists')}`, async () => {
+  test(`should return ${chalk.green(200)} and render post/index page if ${chalk.blue('post exists')}`, async () => {
     const res = await request(app).get('/board?page=1&limit=5');
     expect(res.statusCode).toEqual(200);
     expect(res.header['content-type']).toContain('text/html');
-    expect(res.text).toContain('There is no data to show.');
+    expect(res.text).toContain('test_title');
   });
 
   test(`should return ${chalk.green(200)} and render post/index page with error message if ${chalk.blue('page over 1000')}`, async () => {
@@ -42,5 +51,39 @@ describe('getBoard', () => {
     expect(res.statusCode).toEqual(200);
     expect(res.header['content-type']).toContain('text/html');
     expect(res.text).toContain('test');
+  });
+});
+
+/**
+ * * 게시글 조회 테스트
+ * 1. 게시글 조회 성공
+ * 2. 게시글 조회 실패 (게시글이 없을 경우)
+ */
+describe('getBoardByPostId', () => {
+  let server;
+
+  beforeAll(async () => {
+    server = app.listen(config.get('server.port'));
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  test(`should return ${chalk.green(200)} and render post/read page with post data if ${chalk.blue('post exists')}`, async () => {
+    const res = await request(app)
+      .get(`/board/${1}`)
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.header['content-type']).toContain('text/html');
+    expect(res.text).toContain('test_title');
+  });
+
+  test(`should return ${chalk.yellow(404)} if ${chalk.blue('post is not exists')}`, async () => {
+    const res = await request(app)
+      .get(`/board/${2}`)
+
+    expect(res.statusCode).toEqual(404);
+    expect(res.body.message).toEqual('No data.');
   });
 });
