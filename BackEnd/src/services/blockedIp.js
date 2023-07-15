af459883cfb185212d8a0ea240b4b046bd4ef3f5
[pyola4981] <[pyola4981@naver.com]>
2023-07-15 18:34:17 +0900

    Feat: 서버 공격에 따른 ip 차단 기능 추가

---
+++ BackEnd/src/services/blockedIp.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { BlockedIp } = require('../utils/connect');
+
+
+const postBlockedIp = async (ip) => {
+    return await BlockedIp.create({
+        blocked_ip: ip
+    });
+};
+
+const getBlockedIp = async () => {
+    return await BlockedIp.findAll();
+};
+
+
+module.exports = {
+    postBlockedIp,
+    getBlockedIp,
+};
