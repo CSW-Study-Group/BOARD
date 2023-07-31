1e23bf432ef0165b2cb6a045760ce48195cf72a8
SoN-B <wjdgh2777@naver.com>
2023-07-31 19:40:02 +0900

    Feat: helmet모듈 & ip차단 함수 강화

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const { app, express, bodyParser } = require('./loaders/express');
-const { sequelize, morgan, logger, config, methodOverride, sentry, chalk } = require('./loaders/module');
+const { sequelize, morgan, logger, config, methodOverride, sentry, helmet, chalk } = require('./loaders/module');
 const { blockedIpGet, ipCheck, ipBlock } = require('./loaders/blockedIp')
 
-// 웹 세팅
+// *웹 세팅
 app.use(express.static('../FrontEnd/public'));
 app.set('views', '../FrontEnd/views');
 app.set('view engine', 'ejs');
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'));
 
+// *모니터링 세팅
 sentry.init({
   // 모든 요청 트래킹
   dsn: config.get('server.dsn'),
@@ -26,17 +27,23 @@ app.use(sentry.Handlers.requestHandler()); // 요청정보 캡처
 app.use(sentry.Handlers.tracingHandler()); // 성능정보 캡처
 app.use(sentry.Handlers.errorHandler()); // 에러정보 캡처
 
-app.use(ipBlock); // 서버 공격 감지 후 ip 차단 목록에 추가
-app.use(ipCheck); // 차단 목록에 있는 ip 차단
+// *보안 세팅
+if (config.get('server.status') === 'production') {
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(ipBlock); // 서버 공격 감지 후 ip 차단 목록에 추가
+  app.use(ipCheck); // 차단 목록에 있는 ip 차단
+}
+
 app.use((req, res, next) => {
   morgan(`:method ":url HTTP/:http-version" :status :response-time ms ${req.headers['x-forwarded-for']}`, { stream: logger.stream })(req, res, next);
 });
 
-// 라우팅
+// *라우팅
 const api_router = require('./src/routes');
 
 app.use('/', api_router);
 
+// *서버 실행 & DB 연결
 if (config.get('server.status') !== 'test') {
   blockedIpGet().then(() => {
     app.listen(config.get('server.port'), () => {
